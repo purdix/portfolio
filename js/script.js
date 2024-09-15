7fae7f23462354ea7e666fd508a5661eb2fd1246
purdix <kogulan.sivaneshan@gmail.com>
2024-09-14 20:23:18 -0400

    V2- initial push

--- js/script.js
+++ js/script.js
@@ -1,11 +1,48 @@
+//Show password
+function myFunction() {
+  var x = document.getElementById("pass");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+//Collapsible notes 
+var coll = document.getElementsByClassName("notes-collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+//Nav scroll
+const navbar = document.querySelector('.navbar-default');
+window.onscroll = () => {
+    if (window.scrollY > 100) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+};
+
+/////////////////////
+
 // Scroll up button
 myID = document.getElementById("scrollToTop");
 var myScrollFunc = function() {
   var y = window.scrollY;
   if (y >= 300) {
-    myID.className = "scrollBtn scrollBtn-show"
+    myID.className = "scrollbtn scrollbtn-show"
   } else {
-    myID.className = "scrollBtn"
+    myID.className = "scrollbtn"
   }
 };
 window.addEventListener("scroll", myScrollFunc);
@@ -47,32 +84,39 @@ $(document).ready(function() {
  });
 /////////////////////
 
-// Carousel
- $('.carousel').carousel({
-    interval: false,
-    wrap: false,
+
+// Submenu
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll('.sidenav a');
+    const sections = document.querySelectorAll('section');
+
+    window.addEventListener('scroll', function () {
+        let currentSection = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop - 160; // Adjust based on your scroll-margin-top
+            const sectionBottom = sectionTop + section.offsetHeight;
+            if (pageYOffset >= sectionTop && pageYOffset < sectionBottom) {
+                currentSection = section.getAttribute('id');
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            const sectionId = link.getAttribute('data-section');
+            const subMenu = link.nextElementSibling;
+
+            if (sectionId === currentSection) {
+                link.classList.add('active');
+                if (subMenu && subMenu.classList.contains('submenu')) {
+                    subMenu.style.display = 'block';
+                }
+            } else {
+                if (subMenu && subMenu.classList.contains('submenu')) {
+                    subMenu.style.display = 'none';
+                }
+            }
+        });
+    });
 });
-
-// Tooltips
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
-
-//Disable arrow on first and last slides
-/* checkitem = function() {
- var $this;
-  $this = $("#carousel");
-  if ($("#carousel .carousel-inner .carousel-item:first").hasClass("active")) {
-    $this.children(".carousel-control-prev").hide();
-    $this.children(".carousel-control-next").show();
-  } else if ($("#carousel .carousel-inner .carousel-item:last").hasClass("active")) {
-    $this.children(".carousel-control-next").hide();
-    $this.children(".carousel-control-prev").show();
-  } else {
-    $this.children(".carousel-control-next").show();
-    $this.children(".carousel-control-prev").show();
-  }
-};
-checkitem();
-$("#carousel").on("slid.bs.carousel", "", checkitem); */
 /////////////////////
