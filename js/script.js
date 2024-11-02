c10ac34f9bb06c24716fc787d77e390fcb5cf1f1
purdix <kogulan.sivaneshan@gmail.com>
2024-11-02 16:20:32 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -63,8 +63,10 @@ function initializeImageToggle() {
 // Typewriter effect
 const typewriterContainer = document.querySelector(".typewriter-container");
 const typewriterTextElement = document.getElementById("typewriterText");
-const typingSpeed = 150;
-const deletingSpeed = 150;
+
+const isMobile = window.innerWidth <= 768; // Mobile if width is 768px or less
+const typingSpeed = isMobile ? 200 : 100; // 200ms for mobile, 100ms for desktop
+const deletingSpeed = isMobile ? 200 : 100; // Same speeds for deleting
 const pauseBetweenWords = 4000;
 const words = typewriterContainer.getAttribute("data-words").split(",");
 let wordIndex = 0;
